cf18f6029ee0408e4ce354620570815f9fc13bff
migueljrojas <migueljrojas@gmail.com>
2018-08-26 23:31:56 -0300

    Setup PM2

---
+++ ecosystem.config.js
@@ -0,0 +1,24 @@
+module.exports = {
+  apps : [{
+    name      : 'FundacionParadeportes',
+    script    : './server.js',
+    env: {
+      NODE_ENV: 'development'
+    },
+    env_production : {
+      NODE_ENV: 'production'
+    }
+  }],
+
+  deploy : {
+    production : {
+      user : 'root',
+      host : '66.97.34.214',
+      key  : '~/.ssh/id_rsa.pub',
+      ref  : 'origin/master',
+      repo : 'git@github.com:migueljrojas/fundacion-paradeportes-front.git',
+      path : '/home/admin/web/fundacionparadeportes.org/public_html',
+      'post-deploy' : 'npm install && pm2 reload ecosystem.config.js --env production'
+    }
+  }
+};
