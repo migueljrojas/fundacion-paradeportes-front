db91c2b744d5f72ae7d5fe90cfab14571e2a0373
migueljrojas <migueljrojas@gmail.com>
2018-08-27 07:32:48 -0300

    Added nocors to fetch

--- components/PageWrapper.js
+++ components/PageWrapper.js
@@ -5,25 +5,25 @@ const PageWrapper = Comp => (
   class extends React.Component {
     static async getInitialProps(args) {
 
-      const headerMenuRes = await fetch(`${Config.apiUrl}/wp-json/menus/v1/menus/header-menu`, {mode: 'cors'});
+      const headerMenuRes = await fetch(`${Config.apiUrl}/wp-json/menus/v1/menus/header-menu`, {mode: 'no-cors'});
       const headerMenu = await headerMenuRes.json();
 
-      const contactRes = await fetch(`${Config.apiUrl}/wp-json/wp/v2/modules?slug=contacto`, {mode: 'cors'});
+      const contactRes = await fetch(`${Config.apiUrl}/wp-json/wp/v2/modules?slug=contacto`, {mode: 'no-cors'});
       const contact = await contactRes.json();
 
-      const paradeportesRes = await fetch(`${Config.apiUrl}/wp-json/wp/v2/modules?slug=paradeportes`, {mode: 'cors'});
+      const paradeportesRes = await fetch(`${Config.apiUrl}/wp-json/wp/v2/modules?slug=paradeportes`, {mode: 'no-cors'});
       const paradeportes = await paradeportesRes.json();
 
-      const empresasRes = await fetch(`${Config.apiUrl}/wp-json/wp/v2/modules?slug=empresas`, {mode: 'cors'});
+      const empresasRes = await fetch(`${Config.apiUrl}/wp-json/wp/v2/modules?slug=empresas`, {mode: 'no-cors'});
       const empresas = await empresasRes.json();
 
-      const videosRes = await fetch(`https://www.googleapis.com/youtube/v3/search?key=AIzaSyDVNqeG-8VOwmezDfFJE4AZEuT342eeXZU&channelId=UCvIiiZy4RvR3inRJIAhLh5g&part=snippet,id&order=date&maxResults=15`, {mode: 'cors'});
+      const videosRes = await fetch(`https://www.googleapis.com/youtube/v3/search?key=AIzaSyDVNqeG-8VOwmezDfFJE4AZEuT342eeXZU&channelId=UCvIiiZy4RvR3inRJIAhLh5g&part=snippet,id&order=date&maxResults=15`, {mode: 'no-cors'});
       const videos = await videosRes.json();
 
-      const newsRes = await fetch(`${Config.apiUrl}/wp-json/wp/v2/news?_embed`, {mode: 'cors'});
+      const newsRes = await fetch(`${Config.apiUrl}/wp-json/wp/v2/news?_embed`, {mode: 'no-cors'});
       const news = await newsRes.json();
 
-      const programasRes = await fetch(`${Config.apiUrl}/wp-json/wp/v2/programas?_embed`, {mode: 'cors'});
+      const programasRes = await fetch(`${Config.apiUrl}/wp-json/wp/v2/programas?_embed`, {mode: 'no-cors'});
       const programas = await programasRes.json();
 
       return {
