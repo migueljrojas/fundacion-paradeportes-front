34c758c5cfdc224b9c676a3336d493106bfb0c0a
migueljrojas <migueljrojas@gmail.com>
2018-09-08 17:53:51 -0300

    Adding headers in preflight

--- server.js
+++ server.js
@@ -15,36 +15,60 @@ app
         server.get("/post/:slug", (req, res) => {
             const actualPage = "/post";
             const queryParams = { slug: req.params.slug, apiRoute: "post" };
+            res.header('Access-Control-Allow-Origin', '*');
+            res.header('Access-Control-Allow-Credentials', true);
+            res.header('Access-Control-Allow-Methods', 'POST, GET, PUT, DELETE, OPTIONS');
+            res.header('Access-Control-Allow-Headers', 'Content-Type');
             app.render(req, res, actualPage, queryParams);
         });
 
         server.get("/:slug", (req, res) => {
             const actualPage = "/post";
             const queryParams = { slug: req.params.slug, apiRoute: "page" };
+            res.header('Access-Control-Allow-Origin', '*');
+            res.header('Access-Control-Allow-Credentials', true);
+            res.header('Access-Control-Allow-Methods', 'POST, GET, PUT, DELETE, OPTIONS');
+            res.header('Access-Control-Allow-Headers', 'Content-Type');
             app.render(req, res, actualPage, queryParams);
         });
 
         server.get("/nota/:slug", (req, res) => {
             const actualPage = "/custom";
             const queryParams = { slug: req.params.slug, apiRoute: "news" };
+            res.header('Access-Control-Allow-Origin', '*');
+            res.header('Access-Control-Allow-Credentials', true);
+            res.header('Access-Control-Allow-Methods', 'POST, GET, PUT, DELETE, OPTIONS');
+            res.header('Access-Control-Allow-Headers', 'Content-Type');
             app.render(req, res, actualPage, queryParams);
         });
 
         server.get("/programa/:slug", (req, res) => {
             const actualPage = "/custom";
             const queryParams = { slug: req.params.slug, apiRoute: "programas" };
+            res.header('Access-Control-Allow-Origin', '*');
+            res.header('Access-Control-Allow-Credentials', true);
+            res.header('Access-Control-Allow-Methods', 'POST, GET, PUT, DELETE, OPTIONS');
+            res.header('Access-Control-Allow-Headers', 'Content-Type');
             app.render(req, res, actualPage, queryParams);
         });
 
         server.get("/category/:slug", (req, res) => {
             const actualPage = "/category";
             const queryParams = { slug: req.params.slug };
+            res.header('Access-Control-Allow-Origin', '*');
+            res.header('Access-Control-Allow-Credentials', true);
+            res.header('Access-Control-Allow-Methods', 'POST, GET, PUT, DELETE, OPTIONS');
+            res.header('Access-Control-Allow-Headers', 'Content-Type');
             app.render(req, res, actualPage, queryParams);
         });
 
         server.get("/_preview/:id/:wpnonce", (req, res) => {
             const actualPage = "/preview";
             const queryParams = { id: req.params.id, wpnonce: req.params.wpnonce };
+            res.header('Access-Control-Allow-Origin', '*');
+            res.header('Access-Control-Allow-Credentials', true);
+            res.header('Access-Control-Allow-Methods', 'POST, GET, PUT, DELETE, OPTIONS');
+            res.header('Access-Control-Allow-Headers', 'Content-Type');
             app.render(req, res, actualPage, queryParams);
         });
 
